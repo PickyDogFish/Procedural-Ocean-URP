5437f2af41426ac4ad30820139b796fef8c9a5f8
PickyDogFish <pifko.kosir@gmail.com>
2023-02-26 10:44:13 +0100

    initializing before generating

--- Assets/TerrainSystem/PlantGeneration/GeneratorPreview.cs
+++ Assets/TerrainSystem/PlantGeneration/GeneratorPreview.cs
@@ -8,7 +8,9 @@ namespace PlantGeneration {
         public PlantGenSettings settings;
         // Start is called before the first frame update
         public void Generate() {
+            settings.GetGenerator().Initialize(settings);
             GetComponent<MeshFilter>().mesh = settings.GetGenerator().Generate(settings);
+            GetComponent<MeshRenderer>().material = settings.material;
         }
         public void Clear() {
             GetComponent<MeshFilter>().mesh = null;
